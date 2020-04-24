28e9417b611871b181be8500d673d8a642dec27f
vishnu jha <vishnu2018cs096abesit@gmaill.com>
2020-04-24 15:41:32 +0530

    module 4 assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+const list_name = ['vishnu', 'jha','yaakov sir','jason','paul','jim','payal','simran','rahul', 'mishra', 'John'];
+for(var i=0;i,list_name.length;i++){
+    if(list_name[i][0]=='j'||list_name[i][0]=='J'){
+    console.log('good bye ' + list_name[i]);
+    }
+    else{
+    console.log('Hello ' + list_name[i]);
+    }
+}
