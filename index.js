62c552bdea4c5bfda62c9e780c45e9b62ae9408a
vishnu jha <vishnu2018cs096abesit@gmaill.com>
2020-04-24 16:05:14 +0530

    final

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const list_name = ['vishnu', 'jha','yaakov sir','jason','paul','jim','payal','simran','rahul', 'mishra', 'John'];
+console.log(list_name)
 for(var i=0;i,list_name.length;i++){
     if(list_name[i][0]=='j'||list_name[i][0]=='J'){
     console.log('good bye ' + list_name[i]);
